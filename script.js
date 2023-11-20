20b2ed8f0936dde6933f80297f9ac9d72d365dcc
anaflmm65 <anaflaviadelimamaciel@gmail.com>
2023-11-20 20:23:42 -0300

    Inclusão dos comentários

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
-var arrayCadastro = [
-    {email: "ana", senha: "123"},
-    {email: "lucas", senha: "abc"}
-];
+function cadastroUsuario() {
+    var emailCad = document.getElementById('cademail').value;
+    var senhaCad = document.getElementById('cadsenha').value;
+    var mensagemCad = document.getElementById('mensCad');
+
+    var users = JSON.parse(localStorage.getItem('users')) || [];
+
+    var Existente = users.find(function(user) {
+        return user.emailCad === emailCad;
+    });
+
+    if (Existente) {
+        mensagemCad.innerHTML = 'Erro: Este usuário já está cadastrado.';
+        mensagemCad.style.color = "red";
+        return;
+    }
+
+    var novo = { emailCad: emailCad, senhaCad: senhaCad };
+    users.push(novo);
+
+    localStorage.setItem('users', JSON.stringify(users));
+
+    mensagemCad.style.color = "green";
+    mensagemCad.innerHTML = 'Cadastro realizado com sucesso!';
+}
 
 function Login() {
     var emailLogin = document.getElementById("email").value;
     var senhaLogin = document.getElementById("senha").value;
     var errorMessage = document.getElementById("error-message");
+    var users = JSON.parse(localStorage.getItem('users')) || [];
     var credenciasValidas = false;
 
-    for(var i = 0; i < arrayCadastro.length ; i++){
-        if(arrayCadastro[i].email === emailLogin && arrayCadastro[i].senha === senhaLogin  ){
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].emailCad === emailLogin && users[i].senhaCad === senhaLogin) {
             credenciasValidas = true;
-            break
+            break; 
         }
     }
+    
     if (credenciasValidas) {
         errorMessage.textContent = "Login bem-sucedido!";
         errorMessage.style.color = "green";
@@ -24,3 +47,4 @@ function Login() {
         errorMessage.style.color = "red";
     }
 }
+
